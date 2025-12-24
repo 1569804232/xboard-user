58bd184be280552fa511d3d79b2c181a1de60c2e
1569804232 <30691425+1569804232@users.noreply.github.com>
2025-12-25 02:22:38 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,19 @@
+// API地址
+window.routerBase = 'https://ai.787802.xyz/'
+window.settings = {
+  // 站点名称
+  title: 'AI坤场',
+  // 站点描述
+  description: '你的在线养坤场',
+  assets_path: '/assets',
+  // 主题色
+  theme: {
+    color: 'default', //可选default、blue、black、、darkblue
+  },
+  // 版本号
+  version: '0.1.1-dev',
+  // 登陆背景
+  background_url: '',
+  // 站点LOGO
+  logo: '',
+}
